51e287f84e7156d384210b3f4b897c76229b503d
Ramal Məmmədli <memmedliramal00@gmail.com>
2022-05-25 23:32:08 +0400

    task project added

---
+++ PustokMVC/Data/Models/Slider.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Models
+{
+    public class Slider : BaseEntity
+    {
+        public string Title1 { get; set; }
+        public string Title2 { get; set; }
+        public string Description { get; set; }
+        public string RedirectUrl { get; set; }
+        public string RedirectUrlText { get; set; }
+        public string Image { get; set; }
+    }
+}
---
+++ PustokMVC/PustokMVC/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Data.DAL;
+using Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PustokMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            List<Slider> sliders = _context.Sliders.ToList();
+            return View(sliders);
+        }
+    }
+}
